356b7b5f312bc848804ef4035dec40b66ea5ec48
mahfuzzahmed <m.mahfujahmd@gmail.com>
2025-01-24 13:10:39 +0600

    added kine authentication , protected route and added some styles

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,8 +1,16 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from 'next/navigation'
 
-const page = () => {
+const page = async() => {
+    const { isAuthenticated } = getKindeServerSession();
+    const isUserAuthenticated = await isAuthenticated();
+    if(!isUserAuthenticated){
+        redirect('/api/auth/login')
+
+    }
     return (
         <div className="max-w-6xl mx-auto">
-        <h2>Profile Page</h2>
+            <h2 className="text-3xl text-center mt-6">Welcome to your profile!</h2>
         </div>
     );
 };
